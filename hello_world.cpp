75418abb2d4478c91015c8ad4e7fbe8fd1d4fcea
bushuevasofia <bushuevasofia@mail.ru>
2025-06-02 17:56:36 -0400

    formatted code with clang-format -style=Mozilla

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
 // Главная фукнция,запрашивающая имя и печатающая приветствие.
-int main() {
-    std::string name;
-    std::cout << "Enter your name: ";
-    std::cin >> name;
-    std::cout << "Hello world from @" << name << std::endl;
+int
+main()
+{
+  std::string name;
+  std::cout << "Enter your name: ";
+  std::cin >> name;
+  std::cout << "Hello world from @" << name << std::endl;
 }
